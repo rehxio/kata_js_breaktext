0c6fc758bfe6829a40c29e4f8c1d41de71cce9f5
Fabián Rodriguez Rodriguez <fabianrguezrguez@gmail.com>
2018-07-04 10:01:49 +0100

    red test, Dividen el texto por palabras

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,7 @@
 const { validateText } = require("../src")
 //'hola', 8 = 'hola'
+//'hola', 2 = 'ho\nla'
+//'hola mundo', 7 = 'hola\nmundo'
 
 describe('Recibe un texto y las columnas', () => {
    test('Devuelve el texto sin partir', () => {
@@ -10,4 +12,7 @@ describe('Recibe un texto y las columnas', () => {
       expect(validateText('Hola',2)).toBe('Ho\nla')
    })
    
+   test('Divide el texto por palabras', () => {
+      expect(validateText('hola mundo',7)).toBe('hola\nmundo')
+   })
 }) 
