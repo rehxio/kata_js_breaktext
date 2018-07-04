00c40c7e9c364f0bc6f8521246da7c60a4099772
Fabián Rodriguez Rodriguez <fabianrguezrguez@gmail.com>
2018-07-04 09:45:12 +0100

    red test, Divide el texto en el numero de columnas indicado

--- test/index.test.js
+++ test/index.test.js
@@ -5,4 +5,9 @@ describe('Recibe un texto y las columnas', () => {
    test('Devuelve el texto sin partir', () => {
       expect(validateText('Hola',8)).toBe('Hola')
    })
+   
+   test('Divide el texto en el numero de columnas indicado', () => {
+      expect(validateText('Hola',2)).toBe('Ho\nla')
+   })
+   
 }) 
