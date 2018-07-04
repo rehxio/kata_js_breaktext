ad1233659de41b31ca7de252bea3c100098a1bee
Fabián Rodriguez Rodriguez <fabianrguezrguez@gmail.com>
2018-07-04 10:35:44 +0100

    green test, Dividen el texto por palabras

--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ export const validateText = (text,columNumbers) => {
       return text;
    }
    else {
-      return text.substring(0,columNumbers) + '\n' + text.substring(columNumbers, text.length)
-
+      let words = text.split(' ');
+      let result = '';
+      if (words.length > 1) {
+         words.forEach(word => {
+            result += word + addBreakline(words, word);
+         });
+         return result;
+      } else {
+         return words[0].substring(0,columNumbers) + '\n' + 
+                words[0].substring(columNumbers, words[0].length)
+      }
    }
+}
+
+const addBreakline = (words, word) => {
+   return (word !== words[words.length - 1]) ? '\n' : '';
 }
